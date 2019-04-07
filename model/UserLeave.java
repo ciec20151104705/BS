b05783c6c650aafa18b1d9255db22b7df3fb69a7
20151104705 <574487776@qq.com>
2019-04-07 10:55:10 +0800

    毕设初版
    文档的上传与下载也成功的能运行了，打印功能并没有，论文也写完啦

---
+++ model/UserLeave.java
@@ -0,0 +1,100 @@
+package bysj.net.model;
+
+public class UserLeave {
+	private int id;
+
+	public UserLeave() {
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	private User user;
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public User getUser() {
+		return this.user;
+	}
+
+	private User user1;
+	
+
+	public User getUser1() {
+		return user1;
+	}
+
+	public void setUser1(User user1) {
+		this.user1 = user1;
+	}
+
+	private String s_0;
+	public void setS_0(String s_0) {
+		this.s_0 = s_0;
+	}
+
+	public String getS_0() {
+		return this.s_0;
+	}
+
+	private String s_1;
+	public void setS_1(String s_1) {
+		this.s_1 = s_1;
+	}
+
+	public String getS_1() {
+		return this.s_1;
+	}
+
+	private String s_2;
+	public void setS_2(String s_2) {
+		this.s_2 = s_2;
+	}
+
+	public String getS_2() {
+		return this.s_2;
+	}
+
+	private String s_3;
+	public void setS_3(String s_3) {
+		this.s_3 = s_3;
+	}
+
+	public String getS_3() {
+		return this.s_3;
+	}
+
+	private String s_4;
+	public void setS_4(String s_4) {
+		this.s_4 = s_4;
+	}
+
+	public String getS_4() {
+		return this.s_4;
+	}
+
+	private String s_5;
+	public void setS_5(String s_5) {
+		this.s_5 = s_5;
+	}
+
+	public String getS_5() {
+		return this.s_5;
+	}
+
+	private String s_6;
+	public void setS_6(String s_6) {
+		this.s_6 = s_6;
+	}
+
+	public String getS_6() {
+		return this.s_6;
+	}
+
+}
